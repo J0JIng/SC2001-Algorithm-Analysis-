fac401cd61178458bcbfc00f3249d115b545543e
SimJM <SimJM@users.noreply.github.com>
2023-10-03 20:05:17 +0800

    Remove scripts

--- Project2/runA.sh
+++
@@ -1,8 +0,0 @@
-javac DijkstraA.java
-
-if [ $? -eq 0 ]; then
-  # echo "Compilation successful"
-  java DijkstraA.java < test1.txt
-else
-  echo "Compilation failed"
-fi
--- Project2/runB.sh
+++
@@ -1,8 +0,0 @@
-javac DijkstraB.java
-
-if [ $? -eq 0 ]; then
-  # echo "Compilation successful"
-  java DijkstraB.java < test1.txt
-else
-  echo "Compilation failed"
-fi
